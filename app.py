18fe288c166ad7f8bb4761bd03ba0326f5b58e1b
shepp <shepper@vcu.edu>
2022-02-07 18:31:51 -0500

    - Moved html tables to /templates
    - Created Home.html (homepage) for app with href links to brelor and gottstand tables

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ Will handle the internet-facing side of things eventually (hopefully)
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('Home.html')
+
+@app.route('/brelor',methods=['GET','POST'])
+def show_brelor_table():
+    return render_template('brelor.html')
+
+
+@app.route('/gottstand',methods=['GET','POST'])
+def show_gottstand_table():  # put application's code here
+    return render_template('gottstand.html')
 
 
 if __name__ == '__main__':
